2f6f0387369a904522f4aaca6a296e5e68643cbe
Ruslan <ruslanryscovbluerex@gmail.com>
2023-03-11 23:46:35 +0100

    fix

--- api/index.ts
+++ api/index.ts
@@ -1,22 +1,6 @@
-require("../src/index");
+require('../src/index');
 
-import { webhookCallback } from "grammy";
-import bot from "../src/core/bot";
+import { webhookCallback } from 'grammy';
+import bot from '../src/core/bot';
 
-function runMiddleware(req, res, fn) {
-    return new Promise((resolve, reject) => {
-        fn(req, res, (result) => {
-            if (result instanceof Error) {
-                return reject(result);
-            }
-
-            return resolve(result);
-        });
-    });
-}
-
-async function handler(req, res) {
-    await runMiddleware(req, res, webhookCallback(bot));
-}
-
-export default handler;
+export default webhookCallback(bot, 'http');
